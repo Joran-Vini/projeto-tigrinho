9e66029939bfe9b0c5d58549016acb16339fa1e1
Joran Vinicíus Silveira Lage <joranvinicius@gmail.com>
2024-12-21 17:41:41 -0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from cs50 import SQL
-from flask import Flask, session, redirect, url_for, request
+from flask import Flask, render_template, session, redirect, url_for, request
 
 from help import login_required
 
 app = Flask(__name__)
 app.secret_key = 'Chave secreta!'
+
+@app.route('/')
+def home():
+    return render_template('layout.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['user_id'] = request.form['username']
+        return redirect(url_for('dashboard'))
+    else:
+         return render_template("login.html")
